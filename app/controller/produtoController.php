1283c634b4c27c5c350ce9c1984340cc63e28f0c
Mateus Matos <mateus.m.oliveira2303@gmail.com>
2022-09-29 20:56:38 -0300

    iniciado api rest

---
+++ app/controller/produtoController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\controller; // UTILIZANDO NAMESPACE PARA IDENTIFICAR NOSSA CLASSE
+
+class ProdutoController{
+   public function get(){
+
+   }
+   public function post(){
+
+   }
+   public function put($id){
+
+   }
+   public function delete($id){
+
+   }
+}
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+require_once("vendor/autoload.php");
+use App\controller\ProdutoController;
 //print_r($_GET);
 $rota = !empty($_GET["url"]) ? $_GET["url"] : "nada ainda" ; // VERIFICA SE A URL NAO ESTA VAZIA, SE ESTIVER EXIBE UM TEXTO CASO CONTRARIO ATRIBUI UM CONTEUDO NORMAL
 echo $rota;
@@ -12,6 +14,23 @@ if($rota[0] === "api" ){
     array_shift($rota);
     //echo "<hr>";
     print_r($rota);
+    // $meuProduto = new ProdutoController();
+    // echo "<hr>";
+    // $meuProduto->inicio();
+    // PEGANDO A INFORMACAO DO SERVICO QUE O USUARIO QUER ACESSAR
+    // ORGANIZANDO O CAMINHO PARA DEIXAR COM O MESMO NOME DA CLASSE DA PASTA CONTROLLER
+    //echo "<hr>";
+    if(!file_exists("App\controller\\".$rota[0]."Controller.php")){
+        echo "<hr>";
+        echo "Esse serviço não esta disponivel";
+    }
+    else{
+        $servico = "App\controller\\".ucfirst($rota[0])."Controller";
+        echo "{$servico}";
+        array_shift($rota);
+        $HTTP = $_SERVER["REQUEST_METHOD"]; // ESTA PEGANDO O VERBO HTTP QUE É O METODO DE ENVIO DOS DADOS OU SEJA GET POST PUT DELETE
+    }
+   
 }
 else{
     echo "<hr>";
