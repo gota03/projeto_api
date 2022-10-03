bba34fede43c47b12190c61da482149e2a0fcc9f
Mateus Matos <mateus.m.oliveira2303@gmail.com>
2022-10-03 20:59:33 -0300

    avancando em api

--- app/controller/produtoController.php
+++ app/controller/produtoController.php
@@ -3,15 +3,15 @@ namespace App\controller; // UTILIZANDO NAMESPACE PARA IDENTIFICAR NOSSA CLASSE
 
 class ProdutoController{
    public function get(){
-
+      return "Estou no metodo get";
    }
    public function post(){
-
+      return "Estou no metodo post";
    }
    public function put($id){
-
+      return "Estou no metodo put";
    }
    public function delete($id){
-
+      return "Estou no metodo delete";
    }
 }
--- index.php
+++ index.php
@@ -25,10 +25,21 @@ if($rota[0] === "api" ){
         echo "Esse serviço não esta disponivel";
     }
     else{
-        $servico = "App\controller\\".ucfirst($rota[0])."Controller";
+        $servico = "App\controller\\".ucfirst($rota[0])."Controller"; // ORGANIZANDO O CAMINHO PARA DEIXAR COM O MESMO NOME DA CLASSE DA PASTA CONTROLLER
+        echo "<hr>";
         echo "{$servico}";
         array_shift($rota);
-        $HTTP = $_SERVER["REQUEST_METHOD"]; // ESTA PEGANDO O VERBO HTTP QUE É O METODO DE ENVIO DOS DADOS OU SEJA GET POST PUT DELETE
+        $HTTP = strtolower($_SERVER["REQUEST_METHOD"]); // ESTA PEGANDO O VERBO HTTP QUE É O METODO DE ENVIO DOS DADOS OU SEJA GET POST PUT DELETE
+        echo "<hr>";
+        echo "{$HTTP}";
+        try {
+            array_shift($rota);
+            $resposta = call_user_func_array(array(new $servico, $HTTP), $rota); // ESTAMOS INSTANCIANDO DINAMICAMENTE A CLASSE PRODUTOCONTROLLER E INFORMANDO QUAL METODO DA CLASSE SERA EXECUTADO PASSANDO A VARIAVEL $HTTP, QUE SERA GET,POST, PUT, DELETE. A VARIAVEL ROTA SERA UTILIZADA PARA PASSAR O PARAMETRO PARA O METODO DA NOSSA CLASSE SERIA ALGO COMO DELETE(1) OU PUT(1)
+
+            echo $resposta;
+        } catch (\Exception $error) {
+            
+        }
     }
    
 }
